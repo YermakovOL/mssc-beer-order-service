226aba65e6f2a6c4b62f7804ebd2244920650827
Olexiy <6diamondsex@gmail.com>
2024-04-30 23:13:54 +0300

    BeerServiceImpl refactoring with @Value

--- src/main/java/guru/sfg/beer/order/service/services/beer/BeerServiceImpl.java
+++ src/main/java/guru/sfg/beer/order/service/services/beer/BeerServiceImpl.java
@@ -1,7 +1,9 @@
 package guru.sfg.beer.order.service.services.beer;
 
 import guru.sfg.brewery.model.BeerDto;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.bind.ConstructorBinding;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -12,19 +14,18 @@ import java.util.UUID;
 /**
  * Created by jt on 2019-06-09.
  */
-@ConfigurationProperties(prefix = "sfg.brewery", ignoreUnknownFields = false)
 @Service
 public class BeerServiceImpl implements BeerService {
     public final static String BEER_PATH_V1 = "/api/v1/beer/";
     public final static String BEER_UPC_PATH_V1 = "/api/v1/beerUpc/";
     private final RestTemplate restTemplate;
-
+    @Value("${sfg.brewery.beer-service-host}")
     private String beerServiceHost;
 
     public BeerServiceImpl(RestTemplateBuilder restTemplateBuilder) {
         this.restTemplate = restTemplateBuilder.build();
+        this.beerServiceHost = beerServiceHost; // Constructor injection
     }
-
     @Override
     public Optional<BeerDto> getBeerById(UUID uuid){
         return Optional.of(restTemplate.getForObject(beerServiceHost + BEER_PATH_V1 + uuid.toString(), BeerDto.class));
