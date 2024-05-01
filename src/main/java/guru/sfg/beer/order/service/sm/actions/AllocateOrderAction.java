0cd840d35ff5c5f819f066c9453853f1a67739e1
Olexiy <6diamondsex@gmail.com>
2024-05-01 16:40:12 +0300

    Create Integration Test for New Order to Allocate & Refactoring

--- src/main/java/guru/sfg/beer/order/service/sm/actions/AllocateOrderAction.java
+++ src/main/java/guru/sfg/beer/order/service/sm/actions/AllocateOrderAction.java
@@ -14,6 +14,7 @@ import org.springframework.statemachine.StateContext;
 import org.springframework.statemachine.action.Action;
 import org.springframework.stereotype.Component;
 
+import java.util.Optional;
 import java.util.UUID;
 
 /**
@@ -31,11 +32,13 @@ public class AllocateOrderAction implements Action<BeerOrderStatusEnum, BeerOrde
     @Override
     public void execute(StateContext<BeerOrderStatusEnum, BeerOrderEventEnum> context) {
         String beerOrderId = (String) context.getMessage().getHeaders().get(BeerOrderManagerImpl.ORDER_ID_HEADER);
-        BeerOrder beerOrder = beerOrderRepository.findOneById(UUID.fromString(beerOrderId));
+        Optional<BeerOrder> beerOrderOptional = beerOrderRepository.findById(UUID.fromString(beerOrderId));
+        beerOrderOptional.ifPresent(beerOrder -> {
+            jmsTemplate.convertAndSend(JmsConfig.ALLOCATE_ORDER_QUEUE,
+                    beerOrderMapper.beerOrderToDto(beerOrder));
 
-        jmsTemplate.convertAndSend(JmsConfig.ALLOCATE_ORDER_QUEUE,
-                beerOrderMapper.beerOrderToDto(beerOrder));
-
-        log.debug("Sent Allocation Request for order id: " + beerOrderId);
+            log.debug("Sent Allocation Request for beerOrder id: " + beerOrderId);
+        });
     }
+
 }
